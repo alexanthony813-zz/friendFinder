cf36ba59ec1f1d3d5f7ec8bf38d09265d2712921
Alex Anthony <alexanderanthony813@gmail.com>
2016-04-28 05:50:10 -0700

    Began working on templates

--- dogs/views.py
+++ dogs/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 import urllib2
+import models
 
-# Create your views here.
+# gets all dogs
 def index(request):
-    return HttpResponse("<h2>Dogs<h2>")
+    dogs = models.Dog.filter()
+    return render(request, 'includes/dogsnippet.html', {'dog_info': dogs})
+
+def get_dog(request):
+    dog_ids = request['dog_ids']
+    dogs = models.Dog.filter(pk__in=dog_ids)
+    return render(request, 'includes/dogsnippet.html', {'dog_info': dogs})
