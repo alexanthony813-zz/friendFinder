4c34fcbe41c01abf11c0537642ea1ba4fba49e77
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-02 22:57:22 -0700

    Added search capability, but external API appears to be down

--- dogs/static/js/dogs.js
+++ dogs/static/js/dogs.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
 
+
+  $('#search').on('click', function(){
+    var zip_code = $('#zip-code').val();
+    $.get('/', {zip_code: zip_code}, callback)
+  })
+  
   $('.module-card-button').on('click',function(){
-    $('')
+    $()
   })
 
 
 });
+
+function callback(){
+  setTimeout(function(){
+    console.log('hi')
+  }, 5000)
+}
