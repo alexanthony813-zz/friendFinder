03098e8d4f9e8bf323bf7f7ae6cbb47fb5235170
Alex Anthony <alexanderanthony813@gmail.com>
2016-05-03 12:30:59 -0700

    Consecutive searches now working

--- dogs/static/js/dogs.js
+++ dogs/static/js/dogs.js
@@ -4,21 +4,44 @@ $(document).ready(function(){
   $('#search').on('click', function(){
     var zip_code = $('#zip-code').val();
     $.get('search/', {zip_code: zip_code}, function(data, err){
+      //reduce to object with name-zip key value
+      var oldNames = dogs.reduce(function(prev, curr){
+        prev[curr.name] = curr.zip_code;
+        return prev;
+      }, {});
+
       if(err){
         console.error(err);
       }
-      dogs = data.dogs;
-      var dogNames = dogs.map(function(dog){
-        return dog.name;
-      });
-      var self = this;
-      self.dogNames = dogNames;
 
+      dogs = data.dogs;
+
+      //reduce to object with name-zip key value
+      var nameToZip = dogs.reduce(function(prev, curr){
+        prev[curr.name] = curr.zip_code;
+        return prev;
+      }, {});
+
+      var self = this;
+      self.oldNames = oldNames;
+      self.nameToZip = nameToZip;
+
+      // removes existing dogs that are not in area
       $('.module-card-title').each(function(dog){
         var name = $(this).text();
-        if(self.dogNames.indexOf(name) === -1){
-          // console.log($(this).parent())
+        if(!(name in self.nameToZip)){
           $(this).parent().remove();
+        }
+      });
+
+      // adds response dogs that are not on page
+      console.log(dogs)
+      dogs.forEach(function(dog){
+        var name = dog.name;
+        console.log(dog.name)
+        if(!(name in self.oldNames)){
+          self.nameToZip[name] = dog.zip_code;
+          appendDog(dog)
         }
       });
   });
@@ -28,6 +51,10 @@ $(document).ready(function(){
   $('.module-card-button').on('click',function(){
     $();
   });
-
-
 });
+
+
+function appendDog(dog){
+  console.log($('.module-card-wrap').length)
+  $('.module-card-wrap').append("<div class='module-card'><div class='module-card-title'>"+dog.name+"</div><div class='module-card-meta'><div class='module-card-category'><span class='fa fa-tag'></span><span>" + dog.size+ " " + dog.sex + "</span></div><div class='module-card-author'><span class='fa fa-user'></span><span>" + dog.age + "</span></div></div> <!-- /module-card-meta --><div class='module-card-bottom'><img src='" + dog.profile_photo_url +"' class='module-card-img' /><button class='module-card-button'>See More</button></div></div>")
+}
